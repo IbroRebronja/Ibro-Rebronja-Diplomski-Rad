1eddb3a647150d52b05c5729600880c9b2497ba8
Ibro Rebronja <27772860+IbroRebronja@users.noreply.github.com>
2025-10-05 14:55:17 +0200

    re-added removed code for profile, search, mobile hamburger icon and scroll on mobile view

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+let searchForm=document.querySelector('.search-form')
+document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active')
+    shoppingCart.classList.remove('active')
+    account.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
+let account=document.querySelector('.login-form')
+document.querySelector('#login-btn').onclick=()=>{
+    account.classList.toggle('active')
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
+let navbar=document.querySelector('.navbar')
+document.querySelector('#menu-btn').onclick=()=>{
+    navbar.classList.toggle('active')
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    account.classList.remove('active')
+}
+
+window.onscroll =()=>{
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    account.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
 var swiper = new Swiper(".product-slider", {
     slidesPerView: 3,
     spaceBetween: 25,
